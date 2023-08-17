5b314c0b4912e8f258299c4c2fc3366c2c7e7b92
mahdiemm <mahdiemohammadpour@yahoo.com>
2023-08-17 11:58:46 +0330

    media

--- article/models.py
+++ article/models.py
@@ -6,7 +6,7 @@ class Articles(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add= True)
-    #add in thumbnail
+    image = models.ImageField(default='default.jpg',blank=True)
     #author
 
     def __str__(self):
--- article/views.py
+++ article/views.py
@@ -3,12 +3,15 @@ from . import models
 
 # Create your views here.
 def articles_list(request):
-    articles = models.Articles.objects.all().order_by('date')
+    articles = models.Articles.objects.all().order_by('-date')
 
     args = {'articles':articles} 
     return render(request, 'article/articleslist.html', args)
 
 
 def articles_detail(request, slug):
-    return HttpResponse(slug)
+    #return HttpResponse(slug)
+    article = models.Articles.objects.get(slug=slug)
+    return render(request , 'article/article_detail.html', {'article':article})
+
 
